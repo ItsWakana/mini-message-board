da8d8c1ed761c77d3f5c12effa59e578a2edf375
wakana <wakanawardrobe@gmail.com>
2023-08-31 13:47:09 +0100

    rewrite message controller callbacks using express async handlers

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,40 +1,66 @@
 const Message = require('../models/message');
+const asyncHandler = require("express-async-handler");
 
-const getMessageById = async (req, res, next) => {
-    try {
-        const foundMessage = await Message.findById(req.params.messageId).exec();
-        res.render('singleMessage', { message: foundMessage });
-      } catch(err) {
-        res.send("No message found with this id");
-      }
-}
+// const getMessageById = async (req, res, next) => {
+//     try {
+//         const foundMessage = await Message.findById(req.params.messageId).exec();
+//         res.render('singleMessage', { message: foundMessage });
+//       } catch(err) {
+//         res.send("No message found with this id");
+//       }
+// }
 
-const getAllMessages = async (req, res, next) => {
-    try {
-        const messages = await Message.find({});
-        res.render('index', { title: 'Mini Message Board', messages: messages });
-      } catch (err) {
-        console.log(err);
-        return next(err);
-      }
-}
+const getMessageById = asyncHandler( async (req, res, next) => {
 
-const writeMessageToDb = async (req, res, next) => {
+    const foundMessage = await Message.findById(req.params.messageId).exec();
+    res.render('singleMessage', { message: foundMessage });
+
+});
+
+// const getAllMessages = async (req, res, next) => {
+//     try {
+//         const messages = await Message.find({});
+//         res.render('index', { title: 'Mini Message Board', messages: messages });
+//       } catch (err) {
+//         console.log(err);
+//         return next(err);
+//       }
+// }
+
+const getAllMessages = asyncHandler( async (req, res, next) => {
+    const messages = await Message.find({});
+    res.render('index', { title: 'Mini Message Board', messages: messages});
+})
+
+// const writeMessageToDb = async (req, res, next) => {
+
+//     const { author, message } = req.body;
+
+//     try {
+//       const myMessage = await Message.create({
+//         messageContent: message,
+//         sentAt: new Date(),
+//         author: author
+//       });
+//     } catch (error) {
+//       res.status(500).json({ error: 'An error occurred while saving the message' });
+//     }
+  
+//     res.redirect('/');
+// }
+
+const writeMessageToDb = asyncHandler( async (req, res, next) => {
 
     const { author, message } = req.body;
 
-    try {
-      const myMessage = await Message.create({
+    await Message.create({
         messageContent: message,
         sentAt: new Date(),
         author: author
-      });
-    } catch (error) {
-      res.status(500).json({ error: 'An error occurred while saving the message' });
-    }
-  
+    });
+
     res.redirect('/');
-}
+})
 
 module.exports = {
     getMessageById,
